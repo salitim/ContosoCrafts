bf1d8eba8771c37bbb540baecad6d0b6869af448
salitim <s.lotus69@gmail.com>
2020-09-10 16:46:14 +0200

    feat : adding service

---
+++ ContosoCrafts.Website/Services/JsonFileProductService.cs
@@ -0,0 +1,38 @@
+﻿using ContosoCrafts.Website.Models;
+using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ContosoCrafts.Website.Services
+{
+    public class JsonFileProductService
+    {
+        public JsonFileProductService(IWebHostEnvironment WebHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public IWebHostEnvironment webHostEnvironment { get; }
+
+        private string JsonFileName
+        {
+            get { return Path.Combine(webHostEnvironment.WebRootPath, "data", "product.json"); }
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            using (var jsonFileReader = File.OpenText(JsonFileName))
+            {
+                return JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+            }
+        }
+    }
+}
